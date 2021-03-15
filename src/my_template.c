eb379fd65fb7d12dca6b0d0aee7eacc8de11650d
MyBeanZ_debian <hdqtoni@gmail.com>
2021-03-15 09:09:11 -0500

    Initial commit of accel project

---
+++ src/my_template.c
@@ -0,0 +1,22 @@
+/*
+ *
+ * 
+ * 
+ */
+
+/* libraries */
+
+/* macros */
+
+/* global variables */
+
+/* local/static variables */
+
+/* func. prototypes */
+
+/* =================== MAIN function ================= */
+int main()
+{
+
+    return 0;
+}
